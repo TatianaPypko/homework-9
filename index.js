c70eb8d52e1c5bd67abc2da0c2ec0f65ff3123cc
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-10-13 20:34:59 +0200

    Added homework 9

---
+++ index.js
@@ -0,0 +1,91 @@
+// One
+// let string = "";
+// for (let x = 10; x <= 20; x++) {
+//   const comma = x!= 20 ? "," : '';
+//   string += x + comma;
+// }
+// console.log(string);
+
+// Two
+// for (let y = 10; y <= 20; y++) {
+//   console.log(y * y);
+// }
+
+// Three
+// for (let z = 1; z <= 10; z++) {
+//   console.log(z * 7);
+// }
+
+// // Four
+// let a = 0;
+// let b = 15;
+// let sum = 0;
+// while (a <= b) {
+//   sum += a;
+//   a++;
+// }
+// console.log(sum);
+
+// // Five
+// let number = 1;
+// for (let h = 3; h <= 6; h++) {
+//   number = number * h;
+// }
+// console.log(number);
+
+
+// // Six
+// let e = 1;
+// let m = 500;
+// let sum = 0;
+// while (e <= m) {
+//   sum += e;
+//   e++;
+// }
+// console.log(sum / m);
+
+// // Seven
+// let pairedNumbers = 0;
+// for (let l = 30; l <= 80; l++) {
+//   if (l % 2 == 0) {
+//     pairedNumbers += l;
+//   }
+// }
+// console.log(pairedNumbers);
+
+// // Eight
+// for (let r = 100; r <= 200; r++) {
+//   if (r % 3 == 0) {
+//     console.log(r);
+//   }
+// }
+
+// // Nine
+// const num = +prompt("Enter number", "");
+// if (num > 0) {
+//   let countTwo = 1;
+//   let amountPairDividers = 0;
+//   let sumPairDivides = 0;
+//   while (countTwo <= num) {
+//     if (num % countTwo == 0) console.log(countTwo);
+//     if (countTwo % 2 == 0) {
+//       amountPairDividerst++;
+//       sumPairDivides += countTwo;
+//     }
+//     countTwo++;
+//   }
+//   console.log("Кількість парних дільників -", amountPairDividers);
+//   console.log("Сума парних дільників -", sumPairDivides);
+// } else {
+//   alert("Введіть натуральне число");
+// }
+
+// Ten
+// let table = '';
+// for (let i = 1; i < 11; i++) {
+//   for (let k = 1; k < 11; k++) {
+//     table += i * k + " ";
+//   }
+//   table += "\n";
+// }
+// console.log(table);
